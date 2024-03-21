f092fbd5c8c88a69a0a06d291ddbffb8ced2a7db
Lyle Simmons <lylesimmons125@gmail.com>
2024-03-21 16:29:08 -0400

    lab complete

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+// About.js
+function About() {
+    return (
+      <>
+        <header>
+          {/* Save space for NavBar */}
+        </header>
+        <main>
+          <h1>This is my about component!</h1>
+        </main>
+      </>
+    );
+  };
+  
+  export default About;
